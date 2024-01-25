1e989eaebf24515264db8deafdf89080726a24bf
Alexis-1999 <alexisaguilarg19@gmail.com>
2024-01-24 21:07:55 -0300

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+
+    @Get()
+    helloworld(){
+        return 'Hello World'
+    }
+
+
+}
